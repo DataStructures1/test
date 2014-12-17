fb2836f7a25f77587bab00c9bc78c78e409851c8
Kurt Meyer <kmeyer@thacher.org>
2014-12-17 15:44:37 -0800

    First commit.

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+
+		for (int k = 0; k < 10; k++) {
+		
+			if( k%2 == 0 )
+			    System.out.println( k +  ": Hello World!");  
+			else
+				System.out.println( k + ": Good-bye World");
+		
+			sum = sum + k;		//assignment
+		}
+		
+		System.out.println("The total is " + sum );
+	}
+
+}
