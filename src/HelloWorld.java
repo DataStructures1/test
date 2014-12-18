10b06b14622862dec50889e3609b87a2f37e4281
Kurt Meyer <kmeyer@thacher.org>
2014-12-17 16:08:01 -0800

    hyphen

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		for (int k = 0; k < 10; k++) {
 		
 			if( k%2 == 0 )
-			    System.out.println( k +  ": Goodbye World!");  
+			    System.out.println( k +  ": Good-bye World!");  
 			else
 				System.out.println( k + ": Good-bye World");
 		
