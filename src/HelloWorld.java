a377123fb573267cedbb933142735500a14276c6
Kurt Meyer <kmeyer@thacher.org>
2014-12-17 16:16:27 -0800

    added comma
    it flows better

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		for (int k = 0; k < 10; k++) {
 		
 			if( k%2 == 0 )
-			    System.out.println( k +  ": Hello World!"); 
+			    System.out.println( k +  ": Hello, World!"); 
 			else
 				System.out.println( k + ": Good-bye World");
 		
